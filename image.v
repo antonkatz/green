136e677a4c03a15fa16a03a66b39fb2e0ecfeec3
Anton Kats <antonkats@gmail.com>
2020-08-17 07:33:47 -0300

    Initial finished version of GPS location extraction

--- image.v
+++ image.v
@@ -15,38 +15,31 @@ struct LatLon {
 pub fn (i Image) get_coordinates() ?LatLon {
 	r := os.exec('exiftool -gpslongitude -gpslatitude $i.file_name')?
 	o := r.output 
-	println(o)
 	return extract_from_exif(o)
 }
 
-// struct Degrees {
-// 	deg int
-// 	min int
-// 	sec f64
-// }
 fn extract_from_exif(output string) LatLon {
 	mut re := regex.new()
 	lines := output.split_into_lines()
-	re.compile(r'.*(?P<d>[0-9]*) deg (?P<m>[0-9]*).*')
+	re.compile(r'.*(?P<d>[0-9]*) deg (?P<m>[0-9]+).+(?P<s>[0-9.]+).+([A-Z])+')
 	
 	return LatLon{
-		lat: get_line(mut re, lines[0])
-		lon: get_line(mut re, lines[1])
+		lat: parse_line(mut re, lines[0])
+		lon: parse_line(mut re, lines[1])
 	} 
 }
 
-fn get_line(mut re regex.RE, line string) f64 {
+fn parse_line(mut re regex.RE, line string) f64 {
 	re.match_string(line)
-	println(re.groups)
-	// s_d, e_d := re.get_group('d')
-	d := line[re.groups[0]..re.groups[1]]
-	m := line[re.groups[2]..re.groups[3]]
-	// s := line[re.groups[4]..re.groups[5]]
-	println(d)
-	println(m)
-	return 0.0
+	d := line[re.groups[0]..re.groups[1]].int()
+	m := line[re.groups[2]..re.groups[3]].int()
+	s := line[re.groups[4]..re.groups[5]].f64()
+	geo_d := line[re.groups[6]..re.groups[7]]
+	return radial_to_float(d,m,s,geo_d)
 }
 
-fn radial_to_float(lat_lon []string) {
-
+fn radial_to_float(d, m int, s f64, dir string) f64 {
+	mag := d + (m / 60) + (s / 3600)
+	if dir in ['S', 'W'] {return mag * -1}
+	return mag
 }
